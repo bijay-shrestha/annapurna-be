899e7a1bf0d8da81bb3a7b14db479d9ae4c168c0
bijay-shrestha <8ijay.shrestha@gmail.com>
2022-06-24 13:36:02 -0500

    Completed Annapurna Backend Skeleton.

---
+++ src/main/java/edu/miu/annapurnabe/AnnapurnaBeApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.annapurnabe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnapurnaBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnnapurnaBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/miu/annapurnabe/AnnapurnaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.annapurnabe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnapurnaBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
