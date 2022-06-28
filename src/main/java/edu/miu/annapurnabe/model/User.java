51a7af0d27efcbbc3beca05265443fba4450ad79
bijay-shrestha <8ijay.shrestha@gmail.com>
2022-06-28 16:43:23 -0500

    Fix: Made username, email, password, studentId as nullable false

--- src/main/java/edu/miu/annapurnabe/model/User.java
+++ src/main/java/edu/miu/annapurnabe/model/User.java
@@ -20,13 +20,13 @@ public class User {
     private static final Character DISABLE = 'D';
     private static final Character ACTIVE = 'A';
 
-    @Column(name = "full_name")
+    @Column(name = "full_name", nullable = false)
     private String fullName;
-    @Column(name="email")
+    @Column(name="email", nullable = false)
     private String email;
-    @Column(name="username")
+    @Column(name="username", nullable = false)
     private String username;
-    @Column(name = "password")
+    @Column(name = "password", nullable = false)
     private String password;
     @Column(name = "status")
     private Character status = ACTIVE;
